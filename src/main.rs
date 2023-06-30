8d5bdd14bc3da0516562a4e8316af822175c1ef8
unknown <kaution.burrowes@gmail.com>
2023-07-01 10:22:50 +1200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,42 @@
 mod runes;
 
+use std::{io::{self, Write}, collections::HashMap, os::raw};
+
 fn main() {
     println!("Hello, world!");
+
+    loop {
+        let mut raw_runes = input("Runes: ");
+
+        let elder_futhark = transliterate_runes(
+            raw_runes,
+            &*runes::ELDER_FUTHARK
+        );
+
+        println!("[Elder Futhark]\n{}\n", elder_futhark);
+    }
+}
+
+fn transliterate_runes(
+    message: String,
+    runes: &HashMap<&'static str, &'static str>
+) -> String {
+    let mut result = message.clone();
+
+    for (rune, transliteration) in runes {
+        result = result.replace(rune, transliteration);
+    }
+
+    result
+}
+
+fn input(message: &str) -> String {
+    let mut input = String::new();
+
+    print!("{}", &message);
+    io::stdout().flush().unwrap();
+
+    io::stdin().read_line(&mut input).expect("Failed to read line.");
+
+    input.trim().to_owned()
 }
--- src/runes.rs
+++ src/runes.rs
@@ -1,64 +1,45 @@
+use std::collections::HashMap;
+
 use lazy_static::lazy_static;
 
-struct Letter {
-    rune: &'static str,
-    transliteration: &'static str,
-    ipa: Vec<&'static str>,
+lazy_static! {
+    pub static ref ELDER_FUTHARK: HashMap<
+    &'static str,
+    &'static str
+    > = HashMap::from([
+        ("ᚠ", "f"),
+        ("ᚢ", "u"),
+        ("ᚦ", "þ"),
+        ("ᚨ", "a"),
+        ("ᚱ", "r"),
+        ("ᚲ", "k"),
+        ("ᚷ", "g"),
+        ("ᚹ", "w"),
+        ("ᚺ", "h"),
+        ("ᚻ", "h"),
+        ("ᚾ", "n"),
+        ("ᛁ", "i"),
+        ("ᛃ", "j"),
+        ("ᛇ", "ï"),
+        ("ᛈ", "p"),
+        ("ᛉ", "z"),
+        ("ᛊ", "s"),
+        ("ᛏ", "t"),
+        ("ᛒ", "b"),
+        ("ᛖ", "e"),
+        ("ᛗ", "m"),
+        ("ᛚ", "l"),
+        ("ᛜ", "ŋ"),
+        ("ᛞ", "d"),
+        ("ᛟ", "o"),
+    ]);
+
+    static ref ELDER_FUTHARK_ALT: HashMap<String, String> = HashMap::new();
+    static ref FUTHORC: HashMap<String, String> = HashMap::new();
+    static ref FUTHORC_ALT: HashMap<String, String> = HashMap::new();
+    static ref YOUNGER_FUTHARK: HashMap<String, String> = HashMap::new();
+    static ref YOUNGER_FUTHARK_ALT: HashMap<String, String> = HashMap::new();
+    static ref FUTHORK: HashMap<String, String> = HashMap::new();
+    static ref FUTHORK_ALT: HashMap<String, String> = HashMap::new();
 }
 
-impl Letter {
-    fn new(
-        rune: &'static str,
-        transliteration: &'static str,
-        ipa: Vec<&'static str>
-    ) -> Letter {
-        Letter {
-            rune,
-            transliteration,
-            ipa,
-        }
-    }
-}
-
-struct Alphabet {
-    letters: Vec<Letter>,
-}
-
-impl Alphabet {
-    fn new(letters: Vec<Letter>) -> Alphabet {
-        Alphabet {
-            letters
-        }
-    }
-}
-
-// lazy_static! {
-//     static ref ELDER_FUTHARK: Alphabet = Alphabet::new(
-//         vec!(
-//             Letter::new("ᚠ", "f", vec!("ɸ", "f")),
-//             Letter::new("ᚢ", "u", vec!("u(ː)")),
-//             Letter::new("ᚦ", "þ", vec!("θ", "ð")),
-//             Letter::new("ᚨ", "a", vec!("a(ː)")),
-//             Letter::new("ᚱ", "r", vec!("r")),
-//             Letter::new("ᚲ", "k", vec!("k")),
-//             Letter::new("ᚷ", "g", vec!("g")),
-//             Letter::new("ᚹ", "w", vec!("w")),
-//             Letter::new("ᚺ", "h", vec!("h")),
-//             Letter::new("ᚾ", "n", vec!("n")),
-//             Letter::new("ᛁ", "i", vec!("i(ː)")),
-//             Letter::new("ᛃ", "j", vec!("j")),
-//             Letter::new("ᛇ", "ï", vec!("æː")),
-//             Letter::new("ᛈ", "p", vec!("p")),
-//             Letter::new("ᛉ", "z", vec!("z")),
-//             Letter::new("ᛊ", "s", vec!("s")),
-//             Letter::new("ᛏ", "t", vec!("t")),
-//             Letter::new("ᛒ", "b", vec!("b")),
-//             Letter::new("ᛖ", "e", vec!("e(ː)")),
-//             Letter::new("ᛗ", "m", vec!("m")),
-//             Letter::new("ᛚ", "l", vec!("l")),
-//             Letter::new("ᛜ", "ŋ", vec!("ŋ")),
-//             Letter::new("ᛞ", "d", vec!("d")),
-//             Letter::new("ᛟ", "o", vec!("o(ː)")),
-//         )
-//     );
-// }
