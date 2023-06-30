860ba1193b1f6229dcab6483c93453f2f7797d22
unknown <kaution.burrowes@gmail.com>
2023-07-01 10:41:03 +1200

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,59 @@
 mod runes;
 
-use std::{io::{self, Write}, collections::HashMap, os::raw};
+use std::{io::{self, Write}, collections::HashMap};
 
 fn main() {
-    println!("Hello, world!");
-
     loop {
-        let mut raw_runes = input("Runes: ");
+        let raw_runes = input("Runes: ");
 
         let elder_futhark = transliterate_runes(
-            raw_runes,
+            &raw_runes,
             &*runes::ELDER_FUTHARK
         );
+        let elder_futhark_alt = transliterate_runes(
+            &raw_runes,
+            &*runes::ELDER_FUTHARK_ALT
+        );
+        let futhorc = transliterate_runes(
+            &raw_runes,
+            &*runes::FUTHORC
+        );
+        let futhorc_alt = transliterate_runes(
+            &raw_runes,
+            &*runes::FUTHORC_ALT
+        );
+        let younger_futhark = transliterate_runes(
+            &raw_runes,
+            &*runes::YOUNGER_FUTHARK
+        );
+        let younger_futhark_alt = transliterate_runes(
+            &raw_runes,
+            &*runes::YOUNGER_FUTHARK_ALT
+        );
+        let futhork = transliterate_runes(
+            &raw_runes,
+            &*runes::FUTHORK
+        );
+        let futhork_alt = transliterate_runes(
+            &raw_runes,
+            &*runes::FUTHORK_ALT
+        );
 
-        println!("[Elder Futhark]\n{}\n", elder_futhark);
+        println!("[Elder Futhark / Proto-Germanic]\n— {}", elder_futhark);
+        println!("[Elder Futhark (Alternative)]\n— {}", elder_futhark_alt);
+        println!("[Futhorc / Anglo-Saxon]\n— {}", futhorc);
+        println!("[Futhorc (Alternative)]\n— {}", futhorc_alt);
+        println!("[Younger Futhark / Old Norse]\n— {}", younger_futhark);
+        println!("[Younger Futhark (Alternative)]\n— {}", younger_futhark_alt);
+        println!("[Futhork / Medieval]\n— {}", futhork);
+        println!("[Futhork (Alternative)]\n— {}", futhork_alt);
+
+        println!();
     }
 }
 
 fn transliterate_runes(
-    message: String,
+    message: &String,
     runes: &HashMap<&'static str, &'static str>
 ) -> String {
     let mut result = message.clone();
